73dd20058d9d5eeef9636e49d036b63c9a75afc1
Debnath <anidebnath@deloitte.com>
2018-05-01 17:26:25 +0530

    google tag added

--- src/app/func-interface/func-interface.component.ts
+++ src/app/func-interface/func-interface.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-func-interface',
@@ -11,7 +12,15 @@ export class FuncInterfaceComponent implements OnInit {
   cA = '>';
   oC = '{';
   cC = '}';
-  constructor() { }
+  constructor(private titleService: Title, private meta: Meta) {
+    this.titleService.setTitle('Functional Interface Supplier, Consumer, Predicate');
+    this.meta.addTags([
+      {name: 'description', content: 'Supplier, Consumer and Predicate are most important Functional Interface in Java SE8.'
+       + ' We will discuss each of them to know When and How to use in real life scenario.'},
+      {name: 'author', content: 'java-lovers'},
+      {name: 'keywords', content: 'Supplier, java 8, Consumer, Predicate'}
+    ]);
+   }
 
   ngOnInit() {
   }
--- src/app/func-interface/func-interface1/func-interface1.component.ts
+++ src/app/func-interface/func-interface1/func-interface1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-func-interface1',
@@ -11,7 +12,15 @@ export class FuncInterface1Component implements OnInit {
   cA = '>';
   oC = '{';
   cC = '}';
-  constructor() { }
+  constructor(private titleService: Title, private meta: Meta) {
+    this.titleService.setTitle('Functional Interface Supplier, Consumer, Predicate | Use of Consumer and Predicate with example');
+    this.meta.addTags([
+      {name: 'description', content: 'Supplier, Consumer and Predicate are most important Functional Interface in Java SE8.'
+       + ' We will discuss each of them to know When and How to use in real life scenario.'},
+      {name: 'author', content: 'java-lovers'},
+      {name: 'keywords', content: 'Supplier, java 8, Consumer, Predicate'}
+    ]);
+   }
 
   ngOnInit() {
   }
